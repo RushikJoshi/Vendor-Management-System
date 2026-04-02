d19e15caf1738f9a71388ee315a43408d40df65b
manas suthar <72956701+manassuthar62@users.noreply.github.com>
2026-04-02 15:20:26 +0530

    error fix

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,9 +1,10 @@
 const cloudinary = require("cloudinary").v2;
+const configs = require("./env");
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+    cloud_name: configs.CLOUDINARY_CLOUD_NAME,
+    api_key: configs.CLOUDINARY_API_KEY,
+    api_secret: configs.CLOUDINARY_API_SECRET,
 });
 
 module.exports = cloudinary;
--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -1,11 +1,12 @@
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require('multer');
+const configs = require('../config/env');
 
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_KEY,
-    api_secret: process.env.CLOUD_SECRET,
+    cloud_name: configs.CLOUDINARY_CLOUD_NAME,
+    api_key: configs.CLOUDINARY_API_KEY,
+    api_secret: configs.CLOUDINARY_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
